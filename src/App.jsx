5c0993ebcae83c6a76fae233058f99ce5b411334
Cal <calvin.m71@hotmail.com>
2020-08-23 13:54:14 -0400

    code added

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./components/Card";
+import "./App.sass";
+
+const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
+
+  useEffect(() => {
+    const callOurYelpAPI = async () => {
+      try {
+        const resp = await axios.get("/api/yelp");
+        setPlaces(resp.data);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+        setError(true);
+      }
+    };
+
+    callOurYelpAPI();
+  }, []);
+  return (
+    <div id="app" className="container">
+      <h1 className="title">Wyncode Does Lunch</h1>
+      {isError && <div className="err">Oh no, something went wrong :(</div>}
+      {isLoading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="tile is-ancestor" style={{ flexWrap: "wrap" }}>
+          {places.length &&
+            places.map((place) => <Card key={place.id} place={place} />)}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
